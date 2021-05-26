95882657623c9fa71111e0d43be58b0dd4d52179
DaikiTakayama <daiki.takayama@rakus.co.jp>
2021-05-26 13:33:20 +0900

    first comment

---
+++ src/main/java/jp/co/ex_intermediate/ExIntermediateApplication.java
@@ -0,0 +1,13 @@
+package jp.co.ex_intermediate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExIntermediateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExIntermediateApplication.class, args);
+	}
+
+}
---
+++ src/main/java/jp/co/ex_intermediate/controller/Ex01Controller.java
@@ -0,0 +1,5 @@
+package jp.co.ex_intermediate.controller;
+
+public class Ex01Controller {
+
+}
